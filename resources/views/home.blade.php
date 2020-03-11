724d284bf1b312ddcbbf78fda8b2ad63909330ba
dongming <dongming0401@gmail.comgit config --global user.email dongming0401@gmail.com>
2020-03-10 22:44:26 -0400

    1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,71 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
+            <div class="favor-header">
+                <h2>Your Favorite Car Search</h2>
+            </div>
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Car Type</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                <div class="card-body row">
+                    <div class="any-check col-4">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Any</label>
 
-                    You are logged in!
+                        <ul>
+                            <li>
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Car</label>
+                            </li>
+                            <li>
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Light truck</label>
+                            </li>
+                            <li>
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Minibus</label>
+                            </li>
+                            <li>
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Van</label>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <div class="postby-check col-4">
+                        <label class="form-check-label" for="exampleCheck1">Veichle posted by</label>
+
+                        <ul>
+                            <li>
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Dealer</label>
+                            </li>
+                            <li>
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Individual sellers</label>
+                            </li>
+
+
+                        </ul>
+                    </div>
+                    <div class="ad-check col-4">
+                        <label class="form-check-label" for="exampleCheck1">Ad type</label>
+                        <ul>
+                            <li>
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">For sale</label>
+                            </li>
+                            <li>
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Sold</label>
+                            </li>
+
+
+                        </ul>
+                    </div>
                 </div>
+
             </div>
         </div>
     </div>
